bcda696f67a8185a1334820747f0212688c5c07f
David Bootle <davidtbootle@gmail.com>
2022-08-16 13:43:38 -0400

    npx tool now works and reads the input filenames properly

---
+++ bin/index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+
+// process arguments
+const args = process.argv.slice(2)
+
+if (args.length != 2) {
+    console.error('Two arguments are required.')
+    process.exit(1)
+}
+
+const src = args[0];
+const dest = args[1];
+
+console.debug(`Fetching files from ${src} and compiling them to ${dest}.`)
+
+// verify file/folder exists
+if (!fs.existsSync(src)) {
+    console.error('Input path must be a valid file/directory.')
+    process.exit(1)
+}
+
+console.debug('File/directory exists.')
+
+// get list of filenames
+var srcFiles = []
+
+if (fs.lstatSync(src).isDirectory()) {
+    // if the input is a directory, add all the filenames in it to src_files
+    fileNames = fs.readdirSync(src);
+    for (fileName of fileNames) {
+        srcFiles.push(`${src}/${fileName}`)
+    }
+} else {
+    // if the input is a file, add it to src_files
+    srcFiles.push(src);
+}
+console.log(srcFiles);
